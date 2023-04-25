3cee1235fbd24b1dff99f521c99514c662d68738
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:49:20 +0900

    feat: View lifecycle & binding

--- app/src/main/java/com/example/psychometricsapp/fragment/QuestionFragment.kt
+++ app/src/main/java/com/example/psychometricsapp/fragment/QuestionFragment.kt
@@ -13,6 +13,48 @@ import com.example.psychometricsapp.databinding.FragmentQuestionBinding
 
 
 class QuestionFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+    private var _binding: FragmentQuestionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentQuestionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.btnNext.setOnClickListener(this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment QuestionFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+    }
+
     override fun onClick(view: View?) {
         when(view?.id) {
             R.id.btn_next -> {
