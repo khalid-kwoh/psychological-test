14611d609cb1cad6766c6bf9522446201c41c4ad
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:43:37 +0900

    add: MainFragment

---
+++ app/src/main/java/com/example/psychometricsapp/fragment/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.example.psychometricsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import com.example.psychometricsapp.R
+import androidx.navigation.Navigation
+import com.example.psychometricsapp.databinding.FragmentMainBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
