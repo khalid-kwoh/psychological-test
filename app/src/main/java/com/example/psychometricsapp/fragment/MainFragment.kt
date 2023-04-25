79db8e6f4377d8ed9e3cf9425346c0628e54f6b7
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:47:12 +0900

    feat: View lifecycle & view binding
    Kotlin Android Extensions가 deprecated 되어 databinding과 같은 방법을 사용함

--- app/src/main/java/com/example/psychometricsapp/fragment/MainFragment.kt
+++ app/src/main/java/com/example/psychometricsapp/fragment/MainFragment.kt
@@ -16,3 +16,45 @@ import com.example.psychometricsapp.databinding.FragmentMainBinding
  * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
+
+    lateinit var navController: NavController
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.btnNext.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_questionFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+    }
+}
