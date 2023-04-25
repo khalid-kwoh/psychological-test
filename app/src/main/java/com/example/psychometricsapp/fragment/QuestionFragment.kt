b5600d291237dcbbd81adf49d22fc183108dd915
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:47:35 +0900

    add: Question fragment

---
+++ app/src/main/java/com/example/psychometricsapp/fragment/QuestionFragment.kt
@@ -0,0 +1,15 @@
+package com.example.psychometricsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.psychometricsapp.R
+import com.example.psychometricsapp.databinding.FragmentMainBinding
+import com.example.psychometricsapp.databinding.FragmentQuestionBinding
+
+
+class QuestionFragment : Fragment(), View.OnClickListener {
