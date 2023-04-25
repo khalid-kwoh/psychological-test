fa51a29530022b1ea46aff310550b48ba51e4188
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:51:24 +0900

    add: Selection fragment

---
+++ app/src/main/java/com/example/psychometricsapp/fragment/SelectionFragment.kt
@@ -0,0 +1,14 @@
+package com.example.psychometricsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.psychometricsapp.R
+import com.example.psychometricsapp.databinding.FragmentSelectionBinding
+
+class SelectionFragment : Fragment(), View.OnClickListener {
