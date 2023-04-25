3fe6434354e8188fa42bc2c711bb218bfaf6cb9d
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:49:44 +0900

    add: Result fragment

---
+++ app/src/main/java/com/example/psychometricsapp/fragment/ResultFragment.kt
@@ -0,0 +1,13 @@
+package com.example.psychometricsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.psychometricsapp.R
+import com.example.psychometricsapp.databinding.FragmentResultBinding
+
+class ResultFragment : Fragment() {
