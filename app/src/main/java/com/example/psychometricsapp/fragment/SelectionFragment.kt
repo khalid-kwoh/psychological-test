84558200d35e19092452b76f4df29e85f0dbb075
khalid-kwoh <kwoh@lunasoft.co.kr>
2023-04-25 14:57:05 +0900

    feat: View lifecycle & binding

--- app/src/main/java/com/example/psychometricsapp/fragment/SelectionFragment.kt
+++ app/src/main/java/com/example/psychometricsapp/fragment/SelectionFragment.kt
@@ -12,3 +12,47 @@ import com.example.psychometricsapp.R
 import com.example.psychometricsapp.databinding.FragmentSelectionBinding
 
 class SelectionFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+    private var _binding: FragmentSelectionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSelectionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding.btnBack.setOnClickListener(this)
+        binding.option1.setOnClickListener(this)
+        binding.option2.setOnClickListener(this)
+        binding.option3.setOnClickListener(this)
+        binding.option4.setOnClickListener(this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SelectionFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+    }
